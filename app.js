22ad52f3d44f9af9e72dfa9a6815ea0928936b57
himanshu-sah <itsmehs07@gmail.com>
2022-01-25 14:46:32 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const API_VERSION = "1.0.0";
+
+app.get("/health", (req, res, next) => {
+  res.send({ status: "ok" });
+});
+
+app.get("/version", (req, res, next) => {
+  res.send({ version: API_VERSION });
+});
+
+app.listen(PORT, () => {
+  console.log("app running on port", PORT);
+});
